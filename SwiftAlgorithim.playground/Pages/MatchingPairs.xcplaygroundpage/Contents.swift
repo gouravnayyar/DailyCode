8453923093098c75ac1d7182cfd9fd898c20fc1c
Gourav <gnayyar@vmware.com>
2021-08-24 19:13:45 +0530

    maching pairs

---
+++ SwiftAlgorithim.playground/Pages/MatchingPairs.xcplaygroundpage/Contents.swift
@@ -0,0 +1,76 @@
+import Foundation
+
+/*
+     Given two strings s and t of length N, find the maximum number of possible matching pairs in strings s and t after swapping exactly two characters within s.
+     A swap is switching s[i] and s[j], where s[i] and s[j] denotes the character that is present at the ith and jth index of s, respectively. The matching pairs of the two strings are defined as the number of indices for which s[i] and t[i] are equal.
+     Note: This means you must swap two characters at different indices.
+
+
+ Example 1
+     s = "abcd"
+     t = "adcb"
+     output = 4
+
+ Example 2
+     s = "mno"
+     t = "mno"
+     output = 1
+ */
+/*
+1. if strings are equal then return length - 2
+2.
+*/
+func matchingPairs(s: String, t: String) -> Int {
+    if s == t { return s.count - 2 }
+    let s = [Character](s)
+    let t = [Character](t)
+    var unmachedPairs = Set<Character>()
+    var unmachedInS = Set<Character>()
+    var unmachedInT = Set<Character>()
+    var count = 0
+    var foundPerfectSwap = false
+    var foundPartialSwap = false
+
+    for index in 0..<s.count {
+        if s[index] == t[index] {
+            count += 1
+        }
+
+        if foundPerfectSwap {
+            // No need to keep looking for swaps
+            continue
+        }
+
+        if s[index] != t[index] {
+            unmachedPairs.insert(s[index])
+            unmachedPairs.insert(t[index])
+            unmachedInT.insert(t[index])
+            unmachedInS.insert(s[index])
+        }
+
+        if  unmachedPairs.contains(t[index]), unmachedPairs.contains(s[index]) {
+            foundPerfectSwap = true
+        } else if unmachedInT.contains(s[index]) || unmachedInS.contains(t[index]) {
+            foundPartialSwap = true
+        }
+    }
+
+    if foundPerfectSwap {
+        return count + 2
+    } else if foundPartialSwap {
+        return count + 1
+    }
+
+    return 0
+}
+
+let s1 = "adcb"
+let t1 = "abcd"
+let expected1 = 4
+let output1 = matchingPairs(s: s1, t: t1)
+print("expected = [\(expected1)] and outpot = [\(output1)]")
+
+
+
+
+
